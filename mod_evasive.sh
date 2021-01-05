9c1d44fdf25bd065047f1dba91311125a3754ad1
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2021-01-05 17:36:33 -0300

    Update mod_evasive.sh

--- mod_evasive.sh
+++ mod_evasive.sh
@@ -2,22 +2,25 @@
 
 echo ""
 
-echo "Checking if the module is installed in this server"
 
-if ! httpd -M | grep evasive24_module > /dev/null; then
-   echo -e "Mod Evasive is not installed! Install? (y/n) \c"
-   read
-   if "$REPLY" = "y"; then
-     yum install evasive24_module -y
-
-else
-
+pckarr=(ea-apache24-mod_evasive)
+yum update -y
+for i in  ${pckarr[*]}
+ do
+  isinstalled=$(rpm -q $i)
+  if [ !  "$isinstalled" == "package $i is not installed" ];
+   then
 cp /etc/apache2/conf.d/300-mod_evasive.conf /etc/apache2/conf.d/300-mod_evasive.conf-orig
 sed -i 's/DOSPageCount 4/DOSPageCount 10/g' /etc/apache2/conf.d/300-mod_evasive.conf
 sed -i 's/DOSSiteCount 100/DOSPageCount 150/g' /etc/apache2/conf.d/300-mod_evasive.conf
 sed -i 's/DOSPageInterval 1/DOSPageInterval 2/g' /etc/apache2/conf.d/300-mod_evasive.conf
 sed -i 's/DOSSiteInterval 2/DOSSiteInterval 3/g' /etc/apache2/conf.d/300-mod_evasive.conf
-
-   fi
-fi
+systemctl restart httpd
+echo "Done"
+  else
+    echo $i is not INSTALLED!!!!
+    yum install $i -y
+  fi
+done
+updatedb
 
