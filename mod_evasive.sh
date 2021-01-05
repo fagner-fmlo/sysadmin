63a1f8d7d6f2d645aa58347adecb8c9beeaf7603
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2021-01-05 17:54:37 -0300

    Update mod_evasive.sh

--- mod_evasive.sh
+++ mod_evasive.sh
@@ -2,12 +2,9 @@
 
 echo ""
 
-
-cp /etc/apache2/conf.d/300-mod_evasive.conf /etc/apache2/conf.d/300-mod_evasive.conf-orig
-sed -i 's/DOSPageCount 4/DOSPageCount 10/g' /etc/apache2/conf.d/300-mod_evasive.conf
-sed -i 's/DOSSiteCount 100/DOSPageCount 150/g' /etc/apache2/conf.d/300-mod_evasive.conf
-sed -i 's/DOSPageInterval 1/DOSPageInterval 2/g' /etc/apache2/conf.d/300-mod_evasive.conf
-sed -i 's/DOSSiteInterval 2/DOSSiteInterval 3/g' /etc/apache2/conf.d/300-mod_evasive.conf
+cd /etc/apache2/conf.d/
+mv /etc/apache2/conf.d/300-mod_evasive.conf /etc/apache2/conf.d/300-mod_evasive.conf-orig
+wget https://raw.githubusercontent.com/fagner-fmlo/arquivos/master/300-mod_evasive.conf
 systemctl restart httpd
 
 
